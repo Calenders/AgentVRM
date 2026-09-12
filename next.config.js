7210d3566fdf38b374d76d394d3ae81611857909
Calenders <optc_1747@outlook.com>
2026-09-12 16:56:21 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   assetPrefix: process.env.BASE_PATH || "",
   basePath: process.env.BASE_PATH || "",
-  trailingSlash: true,
   publicRuntimeConfig: {
     root: process.env.BASE_PATH || "",
   },
